18fd3d8bc92bc3f190eccf327a2dce7289ecae00
polin103109 <sabrinpolinece18@gmail.com>
2024-01-02 13:29:49 +0600

    started adding subfolders

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 
 import './App.css'
+import React, { useState } from 'react';
+
+
 
 function App() {
-  
-
+  const [subfoldersVisible, setSubfoldersVisible] = useState(false);
+  const clickFolder = () => {
+    setSubfoldersVisible(!subfoldersVisible);
+  };
   return (
-    <>
+  
+      <div>
+        <div className='folderDiv' onClick={clickFolder}>Folder</div>
+        
+        {subfoldersVisible && (
+        <div className='subfolders'>
+          <div className='subfolder'>Subfolder 1</div>
+          <div className='subfolder'>Subfolder 2</div>
+        
+        </div>
+      )}
+      </div>
     
-    <div className='folderDiv'>Folder</div>
-     
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+   
