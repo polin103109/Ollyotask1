5813e907271fd571abc97576469aec6115c2f103
polin103109 <sabrinpolinece18@gmail.com>
2024-01-02 14:22:38 +0600

    added the changes for UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,61 @@
-
-import './App.css'
 import React, { useState } from 'react';
+import './App.css';
 
-
-
-function App() {
+function Subfolder({ name, subfolders }) {
   const [subfoldersVisible, setSubfoldersVisible] = useState(false);
-  const clickFolder = () => {
+
+  const clickSubfolder = () => {
     setSubfoldersVisible(!subfoldersVisible);
   };
+
   return (
-  
-      <div>
-        <div className='folderDiv' onClick={clickFolder}>Folder</div>
-        
-        {subfoldersVisible && (
+    <div>
+      <div className='subfolder' onClick={clickSubfolder}>
+        {name}
+      </div>
+      {subfoldersVisible && (
         <div className='subfolders'>
-          <div className='subfolder'>Subfolder 1</div>
-          <div className='subfolder'>Subfolder 2</div>
-        
+          {subfolders.map((subfolder, index) => (
+            <div className='subfolder'>
+            <Subfolder key={index} name={subfolder.name} subfolders={subfolder.subfolders} />
+            </div>
+          ))}
         </div>
       )}
+    </div>
+  );
+}
+
+function App() {
+  const [foldersVisible, setFoldersVisible] = useState(false);
+
+  const clickFolder = () => {
+    setFoldersVisible(!foldersVisible);
+  };
+
+  return (
+    <div>
+      <div className='folderDiv' onClick={clickFolder}>
+        Folder
       </div>
-    
+
+      {foldersVisible && (
+        <div className='subfolders'>
+          <Subfolder
+            name='Fruits'
+            subfolders={[
+              { name: 'Apple', subfolders: [{name:'applle',subfolders:[]}] },
+              { name: 'Orange', subfolders: [{ name: 'Orangii', subfolders: [] }] },
+            ]}
+          />
+          <Subfolder name='Animals' subfolders={[
+            { name: 'Lion', subfolders: [] },
+            { name: 'Tiger', subfolders: [{ name: 'Baby Tiger', subfolders: [] }] },
+          ]} />
+        </div>
+      )}
+    </div>
   );
 }
 
 export default App;
-
-   
