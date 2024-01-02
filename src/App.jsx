b6eae2de1ec5fe469744e107d00199c3d4e221fa
Sabrina Sharmin <home@Sabrinas-MacBook-Air.local>
2024-01-02 12:33:03 +0600

    initialize react app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    
+    
+     <div>hiiii</div>
+     <h1 className="underline">My portfolio</h1>
+          <p>Here are the case studies of my projects...</p>
+    </>
+  )
+}
+
+export default App
